28954937de69295259e1dc9319b2818ae65a4ae3
nintyamahayani <79453380+nintyamahayani@users.noreply.github.com>
2021-02-25 23:52:35 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+
+    <head>
+    <title>Tugas Teknologi Web 1</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="with=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="robots" content="index, follow, max-snippet:-1, max-image,preview:large, max-video-preview:-1">
+    <link rel="cannonical" href="">
+    <meta property="og:locale" content="en_US">
+    <meta property="og:type" content="website">
+    <meta property="og:tittle" content="">
+    <meta property="og:description" content="">
+    <meta property="og:url" content="">
+    <meta property="og:site_name" content="">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+
+</head>
+
+<body>
+    <div class="jumbotron text-center">
+        <h1>Sistem Informasi Penyulingan Minyak Cengkeh</h1>
+        <p>(1915101034) Luh Nintya Mahayani</p>
+        <img src="media\minyak.jpg">
+    </div>
+
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-4">
+               <center> <h3>Deskripsi</h3> <center>
+            </div>
+            <div class="col-sm-4">
+                <center> <h3>Khasiat</h3> <center>
+            </div>
+            <div class="col-sm-4">
+               <center> <h3>Bahan Baku</h3> <center>
+            </div>
+            <div class="col-sm-4">
+              <center> <h3>Risiko</h3> <center>
+            </div>
+            <div class="col-sm-4">
+               <center> <h3>Cara Pembuatan<h3> <center>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
+   
