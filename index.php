cf535aa77a3481483ecd045e050ee0bf3bbf74c8
nintyamahayani <79453380+nintyamahayani@users.noreply.github.com>
2021-03-07 14:15:59 +0800

    Add files via upload

--- index.php
+++ index.php
@@ -1,53 +1,76 @@
 <!DOCTYPE html>
 <html lang="en">
-
-    <head>
-    <title>Tugas Teknologi Web 1</title>
-    <meta charset="utf-8">
-    <meta name="viewport" content="with=device-width, initial-scale=1">
-    <meta name="description" content="">
-    <meta name="robots" content="index, follow, max-snippet:-1, max-image,preview:large, max-video-preview:-1">
-    <link rel="cannonical" href="">
-    <meta property="og:locale" content="en_US">
-    <meta property="og:type" content="website">
-    <meta property="og:tittle" content="">
-    <meta property="og:description" content="">
-    <meta property="og:url" content="">
-    <meta property="og:site_name" content="">
-    <link rel="stylesheet" href="assets/css/style.css">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
-
+<head>
+  <title>Genie JH</title>
+<meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+  <link rel="stylesheet" type="text/css" href="assets/css/style.css">
 </head>
-
 <body>
-    <div class="jumbotron text-center">
-        <h1>Sistem Informasi Penyulingan Minyak Cengkeh</h1>
-        <p>(1915101034) Luh Nintya Mahayani</p>
-        <img src="media\minyak.jpg">
-    </div>
 
-    <div class="container">
-        <div class="row">
-            <div class="col-sm-4">
-               <center> <h3>Deskripsi</h3> <center>
-            </div>
-            <div class="col-sm-4">
-                <center> <h3>Khasiat</h3> <center>
-            </div>
-            <div class="col-sm-4">
-               <center> <h3>Bahan Baku</h3> <center>
-            </div>
-            <div class="col-sm-4">
-              <center> <h3>Risiko</h3> <center>
-            </div>
-            <div class="col-sm-4">
-               <center> <h3>Cara Pembuatan<h3> <center>
+<nav class="navbar navbar-expand-sm bg-dark navbar-dark">
+  <a class="navbar-brand" href="#">
+    <img src="media/jh.jpg" alt="jh" style="width:40px;">
+  </a>
+  <ul class="navbar-nav">
+    <li class="nav-item">
+      <a class="nav-link" href="#">Home</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="Bag.html">Bag</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="Wallet.html">Wallet</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="Watches.html">Watches</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="form.html">Sign Up</a>
+    </li>
+  </ul>
+</nav>
+
+    <div class="jumbotorn text-center" style="background-color:pink">
+    <h1>Welcome To Our Customer Jimshoney</h1>
+    <img src="media\smb.jpg" width="400">
+
+  <div class="text-left">
+    <h3>About Us</h3>
+    <h6>Jimshoney merupakan salah satu brand lokal yang ada di Indonesia. Jimshoney menawarkan produk berkualitas, mulai dari tas, dompet, hingga jam tangan untuk pria maupun wanita.</h6>
+    <h6>Produk Jimshoney dibuat menggunakan bahan kulit dengan kualitas terbaik dan tentunya dengan harga yang tidak terlalu menguras kantong.</h6>
+    <h6>Tas casual wanita lebih dari 40 model, dompet model casual seperti dompet kartu, dompet handphone, dompet two in one, dompet kecil dan dompet koin lebih dari 30 model.</h6>
+    <h6>Kemudian jam tangan yang juga memiliki lebih dari 30 model.</h6>
+    <h6>Produk Jimshoney cocok untuk wanita disegala aktivitas baik formal atau santai.</h6>
+  </div>
+
+</section>
+    <section class="slider">
+        <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                    <img class="d-block w-100" src="media\Sheren.jpeg" alt="First slide">
+                </div>
+                <div class="carousel-item">
+                    <img class="d-block w-100" src="media\kirei.jpg" alt="Second slide">
+                </div>
+                <div class="carousel-item">
+                    <img class="d-block w-100" src="media\8123.jpg" alt="Third slide">
+                </div>
+            <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="sr-only">Previous</span>
+            </a>
+            <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="sr-only">Next</span>
+            </a> 
             </div>
         </div>
-    </div>
+    </section>    
 </body>
-
 </html>
-   
